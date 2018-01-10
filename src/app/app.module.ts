27ff2e07ee86af3e96f3c42dec21519e7b70d9c2
nick <vaughnick@gmail.com>
2018-01-10 13:22:55 -0500

    spirit wood init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,41 @@
+
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { NavComponent } from './nav/nav.component';
+import { UsrNavComponent } from './usr-nav/usr-nav.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    FooterComponent,
+    SearchComponent,
+    NavComponent,
+    UsrNavComponent,
+    AccordionComponent,
+    LoginFormComponent,
+    RecipesComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/usr-nav/usr-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usr-nav',
+  templateUrl: './usr-nav.component.html',
+  styleUrls: ['./usr-nav.component.sass']
+})
+export class UsrNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
