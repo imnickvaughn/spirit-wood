df686cfd2bdd397a705d37b063bc502c0f432e6f
nick <vaughnick@gmail.com>
2018-01-10 18:18:42 -0500

    lists

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.sass']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A Test Recipe', 'This is simply a test',
+    'http://www.seriouseats.com/images/2017/09/20170919-stir-fry-recipes-roundup-02.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test',
+    'http://www.seriouseats.com/images/2017/09/20170919-stir-fry-recipes-roundup-02.jpg'),
+  ];
 
   constructor() { }
 
